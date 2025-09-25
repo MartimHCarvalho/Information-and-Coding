262001ebfd462849bda97774d311a0817c864d37
Martim <martimhcarvalho@ua.pt>
2025-09-25 11:44:48 +0100

    histogram improve

--- trab1/part1/wav_quant.cpp
+++ trab1/part1/wav_quant.cpp
@@ -9,7 +9,6 @@ constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 
 int main(int argc, char *argv[])
 {
-
     if (argc < 4)
     {
         cerr << "Usage: " << argv[0] << " <input file> <target_bits> <output_file>\n";
@@ -42,22 +41,26 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int goal_bits { stoi(argv[argc-2]) };
+    int goal_bits{stoi(argv[argc - 2])};
 
-    if (goal_bits < 1 || goal_bits > 16) { 
+    if (goal_bits < 1 || goal_bits > 16)
+    {
         cerr << "Error: invalid number of bits to represent the audio sample\n";
         return 1;
     }
 
     size_t num_bits_to_cut = 16 - goal_bits;
     vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
-    WAVQuant quant { };
+    WAVQuant quant{};
 
     size_t nFrames;
     while ((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE)))
     {
-       samples.resize(nFrames * sndFile.channels());
+        samples.resize(nFrames * sndFile.channels());
         quant.quant(samples, num_bits_to_cut);
     }
     quant.to_Wav(sfhOut);
+    quant.printHistogram();
+
+    return 0;
 }
--- trab1/part1/wav_quant.h
+++ trab1/part1/wav_quant.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
+#include <array>
 #include <sndfile.hh>
 
 class WAVQuant
@@ -21,15 +21,38 @@ public:
     {
         for (auto sample : samples)
         {
-            sample = sample >> num_bits_to_cut; // take each short sample and turn into 0 the num_bits_to_cut least significant bits
-            short tmp = sample << num_bits_to_cut; // shift the sample back to its original position
-            quant_samples.insert(quant_samples.end(), tmp); // add the sample to the vector
+            sample = sample >> num_bits_to_cut;    // Shift right to cut bits
+            short tmp = sample << num_bits_to_cut; // Shift left back to original position with cut bits zeroed
+            quant_samples.push_back(tmp);
         }
     }
 
     void to_Wav(SndfileHandle &sfhOut)
     {
         sfhOut.write(quant_samples.data(), quant_samples.size());
+    }
+
+    std::array<size_t, 256> computeHistogram() const
+    {
+        std::array<size_t, 256> histogram = {};
+        for (auto sample : quant_samples)
+        {
+            size_t bin = static_cast<size_t>((static_cast<int>(sample) + 32768) / 256);
+            if (bin >= histogram.size())
+                bin = histogram.size() - 1;
+            histogram[bin]++;
+        }
+        return histogram;
+    }
+
+    void printHistogram() const
+    {
+        auto histogram = computeHistogram();
+        for (size_t i = 0; i < histogram.size(); i++)
+        {
+            int bin_center = static_cast<int>(i * 256 - 32768 + 128); 
+            std::cout << bin_center << " " << histogram[i] << std::endl;
+        }
     }
 };
 
