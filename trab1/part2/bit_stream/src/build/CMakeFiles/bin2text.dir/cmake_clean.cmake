ee35a3704d314c91cbaec318370e7b907fbaee58
Martim <martimhcarvalho@ua.pt>
2025-10-02 15:06:24 +0100

    clean rep

---
+++ trab1/part2/bit_stream/src/build/CMakeFiles/bin2text.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/martim/MECT/1Ano/1Semestre/IC/Pratical/Information-and-Coding/trab1/part2/bit_stream/bin/bin2text"
+  "/home/martim/MECT/1Ano/1Semestre/IC/Pratical/Information-and-Coding/trab1/part2/bit_stream/bin/bin2text.pdb"
+  "CMakeFiles/bin2text.dir/bin2text.cpp.o"
+  "CMakeFiles/bin2text.dir/bin2text.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin2text.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
